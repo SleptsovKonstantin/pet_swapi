af946678c503174bce8ee5d54fd294bfeeae12df
Константин Слепцов <konstantinslepcov@MacBook-Air-Konstantin.local>
2022-07-19 17:06:43 +0300

    v3

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,47 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import { getApiResourse } from '../../utils/network'
-import { API_PEOPLE } from '../../constans/api'
-import { getPeopleId, getPeopleImage } from '../../services/getPeopleData'
+import React, { useState, useEffect } from "react";
+import { getApiResourse } from "../../utils/network";
+import { API_PEOPLE } from "../../constans/api";
+import { getPeopleId, getPeopleImage } from "../../services/getPeopleData";
+import PeopleList from "../../components/PeoplePage/PeopleList";
 
 // import styles from "./PeoplePage.module.css";
 
 const PeoplePage = () => {
-  const [people, setPeople] = useState(null)
+  const [people, setPeople] = useState(null);
 
   const getResourse = async (url) => {
-    const res = await getApiResourse(url)
+    const res = await getApiResourse(url);
 
     const peopleList = res.results.map(({ name, url }) => {
-      const id = getPeopleId(url)
-      const img = getPeopleImage(id)
-      console.log(img)
+      const id = getPeopleId(url);
+      const img = getPeopleImage(id);
+      // console.log(img);
 
-      return { id, name, img }
-    })
+      return { id, name, img };
+    });
 
-    setPeople(peopleList)
+    setPeople(peopleList);
 
-    console.log('people', people)
-  }
+    console.log("people", people);
+  };
 
   useEffect(() => {
-    getResourse(API_PEOPLE)
-  }, [])
+    getResourse(API_PEOPLE);
+  }, []);
 
-  return (
-    <>
-      {people && (
-        <ul>
-          {people.map(({ id, name, img }) => (
-            <li key={id}>
-                <img src={img} alt={name} />
-                <p>{name}</p>
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
-  )
-}
+  return <>{people && <PeopleList people={people} />}</>;
+};
 
-export default PeoplePage
+export default PeoplePage;
--- src/utils/network.js
+++ src/utils/network.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export const getApiResourse = async (url) => {
   try {
     const res = await axios(url);
-    console.log('res', res);
     return res.data;
   } catch (error) {
     console.error('Could not fetch.', error.message);
